86e70e32056cd18c236bf14afaee5d00daf15d54
Kimia <Kimia@DESKTOP-QBUSUMM>
2024-12-02 09:35:16 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Responsi2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private NpgsqlConnection conn;
+        string connstring = "Host=localhost;Port=5432;Username=postgres;Password=informatika;Database=Responsi2";
+        public DataTable dt;
+        public static NpgsqlCommand cmd;
+        private string sql = null;
+        private DataGridViewRow r;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new NpgsqlConnection(connstring);
+            load();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                r = dataGridView1.Rows[e.RowIndex];
+                textBox1.Text = r.Cells["_karyawan"].Value.ToString();
+                comboBox1.Text = r.Cells["_dep"].Value.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                int id_dep = 0;
+                sql = @"select * from insrt(:_karyawan, :_dep)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_nama",textBox1.Text);
+                if (comboBox1.Text == "HR")
+                    id_dep = 1;
+                if (comboBox1.Text == "ENG")
+                    id_dep = 2;
+                if (comboBox1.Text == "DEV")
+                    id_dep = 3;
+                if (comboBox1.Text == "PM")
+                    id_dep = 4;
+                if (comboBox1.Text == "FIN")
+                    id_dep = 5;
+                cmd.Parameters.AddWithValue("_dep", id_dep);
+                if ((int)cmd.ExecuteScalar() == 1)
+                {
+                    MessageBox.Show("Data Users Berhasil diinputkan", " Well done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    conn.Close();
+                    load();
+                    textBox1.Text = comboBox1.Text = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.Message, "Insert FAIL!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (r == null)
+            {
+                MessageBox.Show("Mohon pilih baris data yang akan diupdate", "Good!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            try
+            {
+                conn.Open();
+                int id_dep = 0;
+                if (comboBox1.Text == "HR")
+                    id_dep = 1;
+                if (comboBox1.Text == "ENG")
+                    id_dep = 2;
+                if (comboBox1.Text == "DEV")
+                    id_dep = 3;
+                if (comboBox1.Text == "PM")
+                    id_dep = 4;
+                if (comboBox1.Text == "FIN")
+                    id_dep = 5;
+                sql = @"select * from updt(:_id,:_karyawan,:_dep)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_id", r.Cells["_id"].Value.ToString());
+                cmd.Parameters.AddWithValue("_karyawan", textBox1.Text);
+                cmd.Parameters.AddWithValue("_dep", id_dep);
+                if ((int)cmd.ExecuteScalar() == 1)
+                {
+                    MessageBox.Show("Data users berhasil diupdate", "Good!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    conn.Close();
+                    textBox1.Text = comboBox1.Text = null;
+                    r = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.Message, "Update FAIL!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (r == null)
+            {
+                MessageBox.Show("Mohon pilih baris data yang akan didelete", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (MessageBox.Show("Apakah bendar anda ingin menghapus data " + r.Cells["_karyawan"].Value.ToString() + " ?", "Hapus data terkonfirmasi",
+                                MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+                try
+                {
+                    conn.Open();
+                    sql = @"select * from dlt(:_id)";
+                    cmd = new NpgsqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("_id", r.Cells["_id"].Value.ToString());
+                    if ((int)cmd.ExecuteScalar() == 1)
+                    {
+                        MessageBox.Show("Data users berhasil dihapus", "Well Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        conn.Close();
+                        load();
+                        textBox1.Text = comboBox1.Text = null;
+                        r = null;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error:" + ex.Message, "Delete FAIL!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+        }
+
+        private void load() 
+        {
+            try
+            {
+                conn.Open();
+                dataGridView1.DataSource = null;
+                sql = "select * from selct()";
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                NpgsqlDataReader rd = cmd.ExecuteReader();
+                dt.Load(rd);
+                dataGridView1.DataSource = dt;
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Fail!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
